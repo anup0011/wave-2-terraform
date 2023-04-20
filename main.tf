cb46d698cceb7c026b610e792a27a7687e9ada7d
Pavangowda111 <130727879+Pavangowda111@users.noreply.github.com>
2023-04-20 13:08:16 +0000

    Update main.tf
    updated service account details

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+resource "google_service_account" "service_account" {
+  account_id   = "vm1-service-account"
+  display_name = "Service Account"
+}
+
 
 resource "google_compute_instance" "default" {
   name         = "virtual-machine1"
