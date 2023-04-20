b0843991b356487ace80795f4293ed129c48bc2b
Pavangowda111 <130727879+Pavangowda111@users.noreply.github.com>
2023-04-20 12:39:02 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,46 +1,30 @@
-resource "google_service_account" "default" {
-  account_id   = "service_account_id"
-  display_name = "Service Account"
-}
 
 resource "google_compute_instance" "default" {
-  name         = "test"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
+  name         = "virtual-machine1"
+  machine_type = "n1-standard-1"
+  zone         = "asia-south2-a"
 
   tags = ["foo", "bar"]
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "value"
-      }
+      image = "windows-cloud/windows-server-2019-dc-core-v20220405"
+     
     }
   }
 
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
-
+  
   network_interface {
     network = "default"
 
     access_config {
-      // Ephemeral public IP
+     
     }
   }
 
   metadata = {
-    foo = "bar"
+   windows-startup-script-ps1 = "New-Item -Path C:\\Windows\\Temp\\hello-world.txt -ItemType File -Value 'Hello, World!'"
+  }
+ tags = ["windows']
   }
 
-  metadata_startup_script = "echo hi > /test.txt"
-
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
-  }
-}
