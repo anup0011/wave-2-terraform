393f064be482b65e1167e307d82a536e6ae2dc87
Anup Abhishek <48859473+anup0011@users.noreply.github.com>
2023-04-20 05:26:44 +0000

    Update gcs.tf

--- gcs.tf
+++ gcs.tf
@@ -2,8 +2,8 @@ resource "random_id" "bucket_prefix" {
   byte_length = 8
 }
 
-resource "google_storage_bucket" "bucket-tfstate" {
-  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+resource "google_storage_bucket" "bucket-test" {
+  name          = "${random_id.bucket_prefix.hex}-bucket-test"
   force_destroy = false
   location      = "ASIA"
   storage_class = "STANDARD"
