179f0df06cf3f24dc86e218ba5214c32a49778b4
srisahithibanda <130641745+srisahithibanda@users.noreply.github.com>
2023-04-21 07:59:18 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+resource "google_service_account" "service_account" {
+  account_id = "vm_service_account"
+  display_name = "Service Account"
+}
+
+resource "google_project_iam_binding" "default" {
+  dinding{
+  project = "db-cicdpipeline-wave-2"
+  role = "roles/compute.instanceAdmin"
+  members = [
+    "serviceAccount:service_account${google_service_account.default.email}"
+  ]
+}
+  resource "google_compute_instance" "windows-vm" {
+  name         = "vm-instance"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "windows-cloud/windows-2022"
+    }
+  }
+  network_interface {
+    network = "custom"
+    subnetwork = "wave2-vm"
+  }
+}
+
+
