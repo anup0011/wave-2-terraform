cc2b09a389ef9893fc7b2f6d0c3e0f86a1c58c64
SahithiNarla <130640469+SahithiNarla@users.noreply.github.com>
2023-04-21 13:07:07 +0000

    Update sa.tf

--- sa.tf
+++ sa.tf
@@ -1,1 +1,9 @@
-
+resource "google_service_account" "sa-name" {
+  account_id="sa-name"
+  display_name="SA"
+}
+resource "google_project_iam_member" "owner_binding" {
+  project="db-cicdpipeline-wave-2"
+  role=roles/compute.instanceAdmin
+  member="serviceAccount:${google_service_account.sa-name.email}"
+}
