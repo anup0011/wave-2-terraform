1c02224ff910d4519ad9212a7f3bc27785937e09
SahithiNarla <130640469+SahithiNarla@users.noreply.github.com>
2023-04-21 13:11:31 +0000

    Update sa.tf

--- sa.tf
+++ sa.tf
@@ -2,8 +2,10 @@ resource "google_service_account" "sa-name" {
   account_id="sa-name"
   display_name="SA"
 }
-resource "google_project_iam_member" "owner_binding" {
+resource "google_project_iam_binding" "owner_binding" {
+  binding{
   project="db-cicdpipeline-wave-2"
   role=roles/compute.instanceAdmin
-  member="serviceAccount:${google_service_account.sa-name.email}"
+  members= ["serviceAccount:${google_service_account.sa-name.email}"]
+    
 }
