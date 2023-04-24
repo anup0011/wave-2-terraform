72980b2f030d9ff9e5dd6ac1720f4a60a3ea4976
ajaykumarshah03 <130749168+ajaykumarshah03@users.noreply.github.com>
2023-04-24 10:40:10 +0530

    Create variables.tf

---
+++ .github/workflows/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  type        = string
+  description = "my_project_tcs_48777"
+
+}
+variable "account_id" {
+  description = "ajayserviceaccount"
+}
+varibale "description" {
+  description = "ajay-sa"
+}
+
+variables "roles" {
+  type         =  list(string)
+  description  = ""
+}
