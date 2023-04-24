f20addc10f300ee8fb04a78ef39d93e19231e1a7
Keerthivashan8087 <130731266+Keerthivashan8087@users.noreply.github.com>
2023-04-24 05:31:08 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  type    = string
+  default = "db-cicdpipeline-wave-2"
+}
+variable "machine_type" {
+  type    = string
+  default = "e2-medium"
+}
+variable "image_linux" {
+  type    = string
+  default = "debian-cloud/debian-11"
+}
+variable "image_windows" {
+  type    = string
+  default = "windows-server-2022-datacenter"
+}
+variable "vm_count" {
+  default = 2
+}
