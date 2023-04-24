e3ca1bbe7969591911821548317aa24bcf0f9072
Neeraja-B <130729267+Neeraja-B@users.noreply.github.com>
2023-04-24 09:38:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,47 @@
+resource "google_compute_instance" "vm_linux" {
+  name         = "linx-vm"
+  machine_type = "e2-medium"
+  zone         = "asia-south2-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network       = "custom"
+    subnetwork    = "wave2-as"
+    access_config { }
+  }
+}
 
+resource "google_compute_instance" "vm_windows1" {
+  name         = "windows-vm1"
+  machine_type = "e2-medium"
+  zone         = "asia-south2-a"
+  boot_disk {
+    initialize_params {
+      image = "windows-server-2022-datacenter"
+    }
+  }
+  network_interface {
+    network       = "custom"
+    subnetwork    = "wave2-as"
+    access_config { }
+  }
+}
+
+resource "google_compute_instance" "vm_windows2" {
+  name         = "windows-vm2"
+  machine_type = "e2-medium"
+  zone         = "asia-south2-a"
+  boot_disk {
+    initialize_params {
+      image = "windows-server-2022-datacenter"
+    }
+  }
+  network_interface {
+    network       = "custom"
+    subnetwork    = "wave2-as"
+    access_config { }
+  }
+}
