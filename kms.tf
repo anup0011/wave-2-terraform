68b86c932eb59e8f48650d803c70179bc4a2e792
Neeraja-B <130729267+Neeraja-B@users.noreply.github.com>
2023-04-26 21:14:22 +0530

    Create kms.tf

---
+++ kms.tf
@@ -0,0 +1,18 @@
+resource "google_kms_key_ring" "keyring" {
+  name     = "keyring"
+  location = "global"
+}
+
+resource "google_kms_crypto_key" "key-crypto" {
+  name            = "key-crypto"
+  key_ring        = google_kms_key_ring.keyring.id
+  
+}
+
+resource "google_kms_crypto_key_iam_binding" "crypto_key" {
+
+  crypto_key_id = google_kms_crypto_key.key-crypto.id
+
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+
+}
