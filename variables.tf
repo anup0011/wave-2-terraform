eccfb71f1d9c132979f533bd498a7c6a37c45c8e
KushalMalla <130637098+KushalMalla@users.noreply.github.com>
2023-04-26 15:17:43 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "region"{
 
 variable "roles"{
  type = list(string)
- default = ["roles/compute.instanceAdmin","rols/cloudkms.cryptoKeyDecrypter"]
+ default = ["roles/compute.instanceAdmin","roles/cloudkms.cryptoKeyEncrypterDecrypter"]
 }
 
 
