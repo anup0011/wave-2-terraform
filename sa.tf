69cc2159bec07a29800cf5af4f1472515ed60ff5
Neeraja-B <130729267+Neeraja-B@users.noreply.github.com>
2023-04-26 16:32:19 +0530

    Update sa.tf

--- sa.tf
+++ sa.tf
@@ -1,1 +1,12 @@
 
+resource "google_service_account" "garage-sa" {
+  account_id   = "service-account"
+  display_name = "Service Account "
+}
+
+resource "google_service_account_iam_member" "gce-account-iam" {
+  service_account_id = google_service_account.garage-sa.name
+  role               = "roles/iam.serviceAccountUser"
+  member             = ["serviceAccount:${google_service_account.garage-sa.email}"
+  ]
+ }
