00f30d5f335bc3145da2b3761a128ec5361942b7
Neeraja-B <130729267+Neeraja-B@users.noreply.github.com>
2023-04-26 21:49:22 +0530

    Update kms.tf

--- kms.tf
+++ kms.tf
@@ -6,5 +6,18 @@ resource "google_kms_key_ring" "keyring" {
 resource "google_kms_crypto_key" "key-crypto" {
   name            = "key-crypto"
   key_ring        = google_kms_key_ring.keyring.id
+  rotation_period = "100000s"
+  lifecycle {
+    prevent_destroy = true
+  }
   
+}
+
+resource "google_kms_crypto_key_iam_binding" "crypto_key" {
+  crypto_key_id = google_kms_crypto_key.key.id
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  members = [
+    "serviceaccount:${google_service_account.garage-sa.email}",
+  ]
+
 }
