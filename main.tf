c960dc7454b0ca5410a4925f63855bcb4dc4a7ee
KushalMalla <130637098+KushalMalla@users.noreply.github.com>
2023-04-27 05:21:02 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ resource "google_compute_instance" "wav2-linux" {
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     email  = "new-service-account@db-cicdpipeline-wave-2.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
   }
 }
 /*resource "google_compute_instance" "wav2-windows" {
