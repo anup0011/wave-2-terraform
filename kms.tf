731bdf3e668f34f98465826cd6c51eeeb7a57048
Neeraja-B <130729267+Neeraja-B@users.noreply.github.com>
2023-04-27 17:12:20 +0000

    Update kms.tf

--- kms.tf
+++ kms.tf
@@ -1,3 +1,7 @@
+resource "google_project_service" "kms_key" {
+  project = "db-cicdpipeline-wave2"
+  service = "cloudkms.googleapis.com"
+}
 resource "google_kms_key_ring" "keyring" {
   name     = "keyring"
   location = "global"
@@ -13,11 +17,3 @@ resource "google_kms_crypto_key" "key-crypto" {
   
 }
 
-resource "google_kms_crypto_key_iam_binding" "crypto_key" {
-  crypto_key_id = google_kms_crypto_key.key-crypto.id
-  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  members = [
-    "serviceaccount:${google_service_account.garage-sa.email}",
-  ]
-
-}
