84130494dea8687d0aa60731e4eb55abdd374131
koshike-sushmitha-tcs <koshike.sushmitha@tcs.com>
2023-04-27 03:21:26 +0000

    update role_binding

--- role_binding.tf
+++ role_binding.tf
@@ -1,6 +1,6 @@
 resource "google_service_account" "wave2-garage-sa" {
-  account_id   = "my-service-account"
-  display_name = "my-service-account"
+  account_id   = "new-service-account"
+  display_name = "new-service-account"
 }
 resource "google_service_account_iam_binding" "sa-account-iam" {
   service_account_id = google_service_account.wave2-garage-sa.name
@@ -8,7 +8,7 @@ resource "google_service_account_iam_binding" "sa-account-iam" {
   role               = each.value
 
   members = [
-    "service_account:my-service-account@${var.project}.iam.gserviceaccount.com",
+    "service_account:new-service-account@${var.project}.iam.gserviceaccount.com",
   ]
 }
 resource "google_kms_key_ring" "keyring-garage" {
