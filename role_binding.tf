8ce040e79fc3f7e8c9c02974ae76d21dbe259ad2
KushalMalla <130637098+KushalMalla@users.noreply.github.com>
2023-04-27 05:15:09 +0000

    Update role_binding.tf

--- role_binding.tf
+++ role_binding.tf
@@ -1,28 +1,28 @@
-resource "google_service_account" "wave2-garage-sa" {
-  account_id   = "new-service-account"
-  display_name = "new-service-account"
-}
-resource "google_service_account_iam_binding" "sa-account-iam" {
-  service_account_id = google_service_account.wave2-garage-sa.name
-  for_each           = toset(var.roles)
-  role               = each.value
+# resource "google_service_account" "wave2-garage-sa" {
+#   account_id   = "new-service-account"
+#   display_name = "new-service-account"
+# }
+# resource "google_service_account_iam_binding" "sa-account-iam" {
+#   service_account_id = google_service_account.wave2-garage-sa.name
+#   for_each           = toset(var.roles)
+#   role               = each.value
 
-  members = [
-    "serviceAccount:new-service-account@${var.project}.iam.gserviceaccount.com",
-  ]
-}
-resource "google_kms_key_ring" "keyring-garage" {
-  name     = "keyring-wave2-garge"
-  location = "global"
-}
+#   members = [
+#     "serviceAccount:new-service-account@${var.project}.iam.gserviceaccount.com",
+#   ]
+# }
+# resource "google_kms_key_ring" "keyring-garage" {
+#   name     = "keyring-wave2-garge"
+#   location = "global"
+# }
 
-resource "google_kms_crypto_key" "key-garage" {
-  name            = "key-wave2-garage"
-  key_ring        = google_kms_key_ring.keyring-garage.id
-  rotation_period = "100000s"
+# resource "google_kms_crypto_key" "key-garage" {
+#   name            = "key-wave2-garage"
+#   key_ring        = google_kms_key_ring.keyring-garage.id
+#   rotation_period = "100000s"
 
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
