bebaba1ca635fad1a934bd1cc2833f2c3023e611
Neeraja-B <130729267+Neeraja-B@users.noreply.github.com>
2023-04-27 16:59:00 +0000

    Update Var.tf

--- Var.tf
+++ Var.tf
@@ -7,3 +7,12 @@ variable "vm_count"{
  type = number
  default = 2
 }
+variable "roles"{
+ type = list(string)
+ default = [
+  "roles/compute.instanceAdmin",
+ "roles/cloudkms.cryptoKeyEncrypterDecrypter",
+ "roles/iam.serviceAccountAdmin",
+ "roles/composer.admin"
+ ]
+}
