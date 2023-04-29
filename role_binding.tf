8b1577d3385c4e732eb3f00e3a07f9cdbf52032f
KushalMalla <kushal.malla@tcs.com>
2023-04-29 17:41:20 +0000

    added roles to members

--- role_binding.tf
+++ role_binding.tf
@@ -3,17 +3,30 @@
   display_name = "test-sa"
 }
 */
-/*resource "google_compute_instance_iam_binding" "instance_binding" {
+resource "google_compute_instance_iam_binding" "instance_binding" {
   depends_on = [ google_compute_instance.wav2-linux ]
   project = var.project
   zone = "asia-south2-b"
   instance_name = google_compute_instance.wav2-linux.name
-  role = "roles/compute.instances.setMetadata"
+  role = "roles/compute.instanceAdmin"
   members = [
     "user:koshike.sushmitha@tcs.com",
-    "user:kushal.malla@tcs.com"
+    "user:kushal.malla@tcs.com",
+    "user:neeraja.balireddy@tcs.com",
+    "user:ajaykumar.shah@tcs.com",
+    "user:apanya.chauhan1@tcs.com",
+    "user:keerthivashan.kc@tcs.com",
+    "user:manushri.m@tcs.com",
+    "user:meetu.singh@tcs.com",
+    "user:nisha.shashi@tcs.com",
+    "user:pavan.gowda@tcs.com",
+    "user:prabin.mohanty@tcs.com",
+    "user:sahithi.narla@tcs.com",
+    "user:sitaramachakravarthy.peruvel@tcs.com",
+    "user:srisahithi.banda@tcs.com",
+    "user:suhasini.dussa@tcs.com"
   ]
-}*/
+}
 
 resource "google_compute_instance_iam_binding" "instance_binding_win" {
   depends_on = [ google_compute_instance.wav2-windows ]
@@ -24,7 +37,20 @@ resource "google_compute_instance_iam_binding" "instance_binding_win" {
   role = "roles/compute.instanceAdmin"
   members = [
     "user:koshike.sushmitha@tcs.com",
-    "user:kushal.malla@tcs.com"
+    "user:kushal.malla@tcs.com",
+    "user:neeraja.balireddy@tcs.com",
+    "user:ajaykumar.shah@tcs.com",
+    "user:apanya.chauhan1@tcs.com",
+    "user:keerthivashan.kc@tcs.com",
+    "user:manushri.m@tcs.com",
+    "user:meetu.singh@tcs.com",
+    "user:nisha.shashi@tcs.com",
+    "user:pavan.gowda@tcs.com",
+    "user:prabin.mohanty@tcs.com",
+    "user:sahithi.narla@tcs.com",
+    "user:sitaramachakravarthy.peruvel@tcs.com",
+    "user:srisahithi.banda@tcs.com",
+    "user:suhasini.dussa@tcs.com"
   ]
 }
 
