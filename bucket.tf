c956fde9d8d4e28ef60fba5fd054711e3c04e7cb
Suhasini69 <130638042+Suhasini69@users.noreply.github.com>
2023-05-03 08:38:25 +0000

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,1 +1,10 @@
-
+resource "google_storage_bucket" "storage-bucket"{
+  name  = var.bucket_name
+  location = "ASIA"
+  storage_class = "STANDARD"
+  force_destroy = false
+  uniform_bucket_level_access = true
+  versioning{
+    enabled = true
+  }
+}
