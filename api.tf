ed337d396257e606612d32f419b68bdba3de6db0
anup0011 <anup0011@gmail.com>
2023-11-21 22:31:21 +0530

    automation

--- api.tf
+++ api.tf
@@ -1,5 +1,23 @@
-resource "google_project_service" "project" {
+resource "google_project_service" "project-compute" {
   project = var.project_id
   service =  "compute.googleapis.com"
+
+}
+
+resource "google_project_service" "project-iam" {
+  project = var.project_id
+  service =  "iam.googleapis.com"
+
+}
+
+resource "google_project_service" "project-cf" {
+  project = var.project_id
+  service =  "cloudfunctions.googleapis.com"
+
+}
+
+resource "google_project_service" "project-schedular" {
+  project = var.project_id
+  service =  "schedular.googleapis.com"
 
 }
