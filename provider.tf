2bf7b0dcfdeae39ce927efeec51225cc712f48c8
anup0011 <anup0011@gmail.com>
2023-11-21 22:15:30 +0530

    automation

--- provider.tf
+++ provider.tf
@@ -1,10 +1,12 @@
 terraform {
-   required_providers {
+  required_providers {
     google = {
       source = "hashicorp/google"
-      version = "4.62.1"
+      version = "5.7.0"
     }
   }
+}
+
  backend "gcs" {
    bucket  = "1010101-terraform-tfstate"
    prefix  = "terraform/state"
