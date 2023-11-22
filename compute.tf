e19210f4229f708b74f91da291e6b4eb8c1105c6
anup0011 <anup0011@gmail.com>
2023-11-22 08:56:16 +0530

    automation

---
+++ compute.tf
@@ -0,0 +1,38 @@
+
+resource "google_compute_instance" "vm1" {
+  name         = "vm1"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["vm1"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = google_compute_network.custom_vpc.id
+	subnetwork = google_compute_subnetwork.custom_subnetwork.id
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.service_account_compute.email
+    scopes = ["cloud-platform"]
+  }
+}
--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,13 @@
-resource "google_compute_network" "custom-vpc" {
+resource "google_compute_network" "custom_vpc" {
   name                    = "test"
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "custom-subnetwork" {
+resource "google_compute_subnetwork" "custom_subnetwork" {
   name          = "uc1"
   ip_cidr_range = "192.168.1.0/24"
   region        = "us-central1"
-  network       = google_compute_network.custom-vpc.id
+  network       = google_compute_network.custom_vpc.id
 
 }
 
