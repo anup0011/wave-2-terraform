8cd54a6aac56d5423918a92857dc476cf90094c1
anup0011 <anup0011@gmail.com>
2023-11-22 08:43:14 +0530

    automation

--- sa.tf
+++ sa.tf
@@ -1,14 +1,14 @@
 resource "google_service_account" "service_account_compute" {
-  account_id   = "cs"
+  account_id   = "compute-sa"
   display_name = "Service Account for compute service"
 }
 
 resource "google_service_account" "service_account_cf" {
-  account_id   = "cf"
+  account_id   = "cloudfunction-sa"
   display_name = "Service Account for cloudfunction"
 }
 
 resource "google_service_account" "service_account_pubsub" {
-  account_id   = "pubsub"
+  account_id   = "pubsub-sa"
   display_name = "Service Account for pubsub"
 }
