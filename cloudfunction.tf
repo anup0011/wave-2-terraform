23cd04db3afb8f36112e0f57b18beda79ec3c494
Anup Abhishek <48859473+anup0011@users.noreply.github.com>
2023-11-22 06:11:57 +0000

    Create cloudfunction.tf

---
+++ cloudfunction.tf
@@ -0,0 +1,56 @@
+
+resource "google_storage_bucket" "bucket" {
+  name     = "${var.project_id}-gcf-source"  # Every bucket name must be globally unique
+  location = "US"
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_object" "object" {
+  name   = "function-source.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "function-source.zip"  # Add path to the zipped function source code
+}
+
+
+
+resource "google_cloudfunctions2_function" "function" {
+  name = "start-stop"
+  location = "us-central1"
+  description = "cloud function to start stop vm "
+
+  build_config {
+    runtime = "python83"
+    entry_point = "startstop"  # Set the entry point 
+    environment_variables = {
+        BUILD_CONFIG_TEST = "build_test"
+    }
+    source {
+      storage_source {
+        bucket = google_storage_bucket.bucket.name
+        object = google_storage_bucket_object.object.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count  = 3
+    min_instance_count = 1
+    available_memory    = "1Gi"
+    timeout_seconds     = 60
+    max_instance_request_concurrency = 80
+    available_cpu = "1"
+    environment_variables = {
+        SERVICE_CONFIG_TEST = "config_test"
+    }
+    ingress_settings = "ALLOW_INTERNAL_ONLY"
+    all_traffic_on_latest_revision = true
+    service_account_email = google_service_account.service_account_cf.email
+  }
+
+  event_trigger {
+    trigger_region = "us-central1"
+    event_type = "google.cloud.pubsub.topic.v1.messagePublished"
+    pubsub_topic = google_pubsub_topic.topic.id
+    retry_policy = "RETRY_POLICY_RETRY"
+  }
+}
